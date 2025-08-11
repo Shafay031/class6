f3c460deb27c368c6533ca308d4bca40fb4bffeb
Shafay <123495561+Shafay031@users.noreply.github.com>
2025-08-11 20:24:49 +0500

    Add files via upload

---
+++ class6.c
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <stdio.h>
+
+int main() {
+  char name[50];
+    printf("what is your name\n");
+    scanf("%s", name);
+   if(name == "shafay"){
+   printf("you are eligible");
+   }else{
+       printf("you are not eligible");
+   }
+    return 0;
+}
